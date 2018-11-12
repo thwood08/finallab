6f1103d60c69b79d393a6db2c5d85c10521de9ca
dsuclassprojects <sharmila.paul@trojans.dsu.edu>
2018-11-11 19:19:32 -0500

    added change in new branch

--- buggy.c
+++ buggy.c
@@ -7,7 +7,7 @@
 3. create a new branch on git
 4. git push changed buggy.c
 5. create pull request with your name and changes you made
-6. screenshot what you did and post it in the dropbox
+6. Send a screenshot of your pull request and put it in the dropbox
 
 */
 
