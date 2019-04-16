2427a8e20b7313209fccc0697c23a04c463a65d2
thwood08 <thwood08@gmail.com>
2019-04-15 21:03:56 -0400

    Fixed buggy.c thwood

--- buggy.c
+++ buggy.c
@@ -10,28 +10,28 @@
 
 */
 
-struct dog
+struct dog      // creates dog as a struct
 {
-	int age;
-	char name = [10];
+	int age;    // initializes age
+	char name[10]; //initializes name with an size allotment of 10
 };
 
 
 void main()
 {
-	struct (dog) thing1:
-	struct dog thing2;
+	struct dog thing1;   //uses struct to create thing1
+	struct dog thing2;   //uses struct to create thing2
 
-	thing1.age = [5];
-	thing2.age = 13;
+	thing1.age = 5;	     //sets thing1 age to 5
+	thing2.age = 13;     //sets thing2 age to 13
 	
-	puts{"What is your dogs name? "};
-	gets(thing1.names);
+	puts("What is your dogs name? ");  //uses std output to ask your dogs name
+	gets(thing1.name);                 //stores the std input as thing1.name
 
-	strcpy(thing2.name, "Buster");
+	strcpy(thing2.name, "Buster");     //uses strcpy to set thing2.name to Buster
 
-	printf("Name: %i\n",thing1.name );
-	printf("Age: %i\n",thing1.age):
-	printf("Thing2 Name: %s\n,thing2.name);
-	printf("Thing2 Age %i\n',thing2.age);
+	printf("Name: %s\n",thing1.name);  //print statement
+	printf("Age: %i\n",thing1.age) ;    //print statement
+	printf("Thing2 Name: %s\n",thing2.name);  //print statement
+	printf("Thing2 Age: %i\n",thing2.age);  //print statement
 }
